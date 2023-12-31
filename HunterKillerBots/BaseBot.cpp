e6831e9a8627b713df59b73169a050ceae1c4ddf
Anton Valkenberg <anton.valkenberg@gmail.com>
2023-12-31 14:41:41 +0100

    Compiling HunterKillerBots project.

--- HunterKillerBots/BaseBot.cpp
+++ HunterKillerBots/BaseBot.cpp
@@ -39,43 +39,51 @@ StructureOrder* BaseBot::GetRandomOrder(const HunterKillerState& rState, const S
 
 void BaseBot::FilterFriendlyFire(std::vector<UnitOrder*>& rOrders, Unit* pUnit, HunterKillerMap& rMap, bool includeInfectedAllyAttacks, bool includeMedicAllySpecialAttacks) {
 
-	const auto removableOrders = std::ranges::remove_if(rOrders.begin(), rOrders.end(), [pUnit, rMap, includeInfectedAllyAttacks, includeMedicAllySpecialAttacks](const UnitOrder* pOrder)
+	const auto removableOrders = std::ranges::remove_if(rOrders.begin(), rOrders.end(), [pUnit, rMap, includeInfectedAllyAttacks, includeMedicAllySpecialAttacks](UnitOrder* pOrder)
 	{
+		auto* pTargetedUnitOrder = dynamic_cast<TargetedUnitOrder*>(pOrder);
 		// Skip non-attack orders
-		if (!pOrder->IsAttackOrder())
+		if (!pTargetedUnitOrder || !pTargetedUnitOrder->IsAttackOrder())
 			return false;
-		// Remove all attacks without a proper target
-		if (rMap.IsAttackOrderWithoutTarget(*pOrder))
-		{
-			delete pOrder;
+		if (IsTargetedOrderFriendlyFire(pTargetedUnitOrder, pUnit, rMap, includeInfectedAllyAttacks, includeMedicAllySpecialAttacks)) {
+			delete pTargetedUnitOrder;
 			return true;
 		}
-		// Remove all attacks with our own location as target
-		if (pOrder->GetTargetLocation().has_value() && pOrder->GetTargetLocation().value() == pUnit->GetLocation())
-		{
-			delete pOrder;
-			return true;
-		}
-		// Remove all attack with an ally structure as target
-		if (rMap.IsAttackOrderTargetingAllyStructure(*pOrder, pUnit))
-		{
-			delete pOrder;
-			return true;
-		}
-		// Remove all attacks with an ally unit as target
-		if (rMap.IsAttackOrderTargetingAllyUnit(*pOrder, pUnit))
-		{
-		    // Unless the order is a for an Infected, or a Medic's special attack
-			const bool goodSelfOrder = (includeInfectedAllyAttacks && pUnit->GetType() == UNIT_INFECTED) ||	(includeMedicAllySpecialAttacks && pUnit->GetType() == UNIT_MEDIC && pOrder->GetOrderType() == ATTACK_SPECIAL);
-            if (!goodSelfOrder)
-			{
-				delete pOrder;
-				return true;
-			}
-		}
-		// Other orders are OK
 		return false;
 	});
 
 	rOrders.erase(removableOrders.begin(), removableOrders.end());
+}
+
+void BaseBot::FilterFriendlyFire(std::vector<TargetedUnitOrder*>& rTargetedOrders, Unit* pUnit, HunterKillerMap& rMap, bool includeInfectedAllyAttacks, bool includeMedicAllySpecialAttacks) {
+
+	const auto removableOrders = std::ranges::remove_if(rTargetedOrders.begin(), rTargetedOrders.end(), [pUnit, rMap, includeInfectedAllyAttacks, includeMedicAllySpecialAttacks](TargetedUnitOrder* pTargetedOrder)
+	{
+		if (IsTargetedOrderFriendlyFire(pTargetedOrder, pUnit, rMap, includeInfectedAllyAttacks, includeMedicAllySpecialAttacks)) {
+			delete pTargetedOrder;
+			return true;
+		}
+		return false;
+	});
+
+	rTargetedOrders.erase(removableOrders.begin(), removableOrders.end());
+}
+
+bool BaseBot::IsTargetedOrderFriendlyFire(TargetedUnitOrder* pOrder, Unit* pUnit, const HunterKillerMap& rMap, bool includeInfectedAllyAttacks, bool includeMedicAllySpecialAttacks)
+{
+	// Remove all attacks with our own location as target
+	if (pOrder->GetTargetLocation() == pUnit->GetLocation())
+		return true;
+	// Remove all attack with an ally structure as target
+	if (rMap.IsAttackOrderTargetingAllyStructure(*pOrder, pUnit))
+		return true;
+	// Remove all attacks with an ally unit as target
+	if (rMap.IsAttackOrderTargetingAllyUnit(*pOrder, pUnit))
+	{
+		// Unless the order is a for an Infected, or a Medic's special attack
+		const bool goodSelfOrder = (includeInfectedAllyAttacks && pUnit->GetType() == UNIT_INFECTED) ||	(includeMedicAllySpecialAttacks && pUnit->GetType() == UNIT_MEDIC && pOrder->GetOrderType() == ATTACK_SPECIAL);
+        return !goodSelfOrder;
+	}
+	// Other orders are OK
+	return false;
 }
